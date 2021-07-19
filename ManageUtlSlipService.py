aad3ac03d67f9be617939997b1d8030e76e0c2a7
heed159 <heed159@naver.com>
2021-07-19 13:14:53 +0900

    수협산지조합 창고 품목별 입출고 현황 조회

---
+++ ManageUtlSlipService.py
@@ -0,0 +1,52 @@
+#-*- coding:utf-8 -*-
+import urllib.request
+import json
+import time
+
+from sqlalchemy import create_engine, engine
+from pandas import json_normalize
+from urllib.error import HTTPError
+
+engine = create_engine("postgresql://127.0.0.1:5432/postgres?user=postgres&password=1234")
+
+def get_response(request):
+    try:
+        response = urllib.request.urlopen(request)
+        return response
+    except HTTPError as ex:
+        for i in range(5):
+            print("{}차 재시도 전 대기중...".format(i+1))
+            time.sleep(60)
+            try:
+                response = urllib.request.urlopen(request)
+                print("{}차 재시도 성공".format(i+1))
+                return response
+            except HTTPError as ex:
+                print("{}차 재시도 실패".format(i+1))
+                if(i==4):
+                    return None
+
+def main():
+    pageNo = 0
+    maxPage = 10
+    serviceKey = "SEgDr%2FYfqIy3tcVNcNig53XdZI1%2FH4ab1uvtyOvmZscb1FgQqDvCansKw32gueJ75vcmMLPnYK%2FBWKYRTlGKAw%3D%3D"
+    while(pageNo < maxPage):
+        pageNo += 1
+        url = "http://apis.data.go.kr/1192000/openapi/service/ManageUtlSlipService/getUtlSlipList?ServiceKey={}&pageNo={}&numOfRows=10&type=json&baseDt=20160301".format(serviceKey, pageNo)
+        request = urllib.request.Request(url)
+        response = get_response(request)
+        rescode = response.getcode()
+        if(rescode==200):
+            try:
+                response_body = json.loads(response.read())
+                maxPage = response_body['responseJson']['header']['totalCount'] // response_body['responseJson']['header']['numOfRows'] + 1
+                datalist = response_body['responseJson']['body']['item']
+                df = json_normalize(datalist)
+                df.to_sql("ManageUtlSlipService", engine, if_exists='append', index=False, chunksize=1000)
+                print("{}/{} 페이지 수집/적재 성공".format(pageNo, maxPage))
+            except Exception as e:
+                print("{}/{} 페이지 수집/적재 실패".format(pageNo, maxPage))
+        else:
+            print("{}/{} 페이지 수집/적재 실패".format(pageNo, maxPage))
+if __name__ : "__main__"
+main()
