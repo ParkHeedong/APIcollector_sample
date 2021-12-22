092d2127140f7a9a6a0b5a2a2070310d837ac9d4
ParkHeedong <heed159@naver.com>
2021-12-22 11:18:46 +0900

    해양수산부_위험물정보_xmltodict_서비스키 삭제

--- getDangerInfo.py
+++ getDangerInfo.py
@@ -30,7 +30,7 @@ def get_response(request):
 def main():
     pageNo = 0
     maxPage = 10
-    serviceKey = "SEgDr%2FYfqIy3tcVNcNig53XdZI1%2FH4ab1uvtyOvmZscb1FgQqDvCansKw32gueJ75vcmMLPnYK%2FBWKYRTlGKAw%3D%3D"
+    serviceKey = "key"
     while(pageNo < maxPage):
         pageNo += 1
         url = "http://apis.data.go.kr/1192000/DgstInqire3/Info?serviceKey={}&stdt=2014&pageNo={}&numOfRows=50".format(serviceKey, pageNo)
